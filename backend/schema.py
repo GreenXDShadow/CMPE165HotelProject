9d4549bb4b6864228a23cc80fb7005775f22c7ee
EricksonNg <52866215+EricksonNg@users.noreply.github.com>
2024-09-14 14:47:56 -0700

    Schema for 'Hotels' and 'Hotel Rooms' added

--- backend/schema.py
+++ backend/schema.py
@@ -21,15 +21,35 @@ class User(db.Model):
     last_name = db.Column('Last Name', db.String)
     reward_points = db.Column('Reward Points', db.Integer)
 
+# Hotels Table
+class Hotel(db.Model):
+    tablename = 'Hotels'
+    hotelid = db.Column('Hotel ID', db.Integer, primarykey=True)
+    longitude = db.Column('Longitude', db.Float)  # can be used with latitude to decipher hotel's location 
+    latitude = db.Column('Latitude', db.Float)
+    name = db.Column('Name', db.String, nullable=False)
+    address = db.Column('Address', db.String)
+    room_count = db.Column('Room Count', db.Integer)
+    rating = db.Column('Rating', db.Float)
+    check_in_start = db.Column('Check In Start', db.String)
+    check_in_end = db.Column('Check In End', db.String)
+    check_out_time = db.Column('Check Out Time', db.String)
+
+# Hotel Rooms Table
+class HotelRoom(db.Model):
+    __tablename = 'Hotel Rooms'
+    hotel_id = db.Column('Hotel ID', db.Integer, db.ForeignKey('Hotels.Hotel ID'), primary_key=True)
+    floor = db.Column('Floor', db.Integer)
+    room_number = db.Column('Room Number', db.Integer, primary_key=True)
+    beds = db.Column('Bed Count', db.String)  # Count is type String to include bed type, 1 King bed would be 1K, 2 Queens is 2Q
 
 # Bookings Table
 class Booking(db.Model):
     __tablename__ = 'Bookings'
     booking_id = db.Column('Booking ID', db.Integer, primary_key=True)
+    room_number = db.Column('Room Number', db.Integer, primary_key=True)
     user_id = db.Column('User ID', db.Integer, db.ForeignKey('Users.User ID'), nullable=False)
     hotel_id = db.Column('Hotel ID', db.Integer, nullable=False)
-    hotel_name = db.Column('Hotel Name', db.String, nullable=False)
-    address = db.Column('Address', db.String)
     check_in_start = db.Column('Check In Start', db.String)  # No need for check-in ending time
     check_out_time = db.Column('Check Out Time', db.String)
     email = db.Column('Email', db.String, nullable=False)
@@ -37,14 +57,11 @@ class Booking(db.Model):
     last_name = db.Column('Last Name', db.String, nullable=False)
     start_date = db.Column('Start Date', db.String, nullable=False)
     end_date = db.Column('End Date', db.String, nullable=False)
-    bed_count = db.Column('Bed Count', db.String)  # Count is type String to include bed type, 1 King bed would be 1K, 2 Queens is 2Q
     nights = db.Column('Nights', db.Integer)
     pricing_per_night = db.Column('Pricing', db.Float)
     cost = db.Column('Cost', db.Float)
     payment_id = db.Column('Payment ID', db.String, db.ForeignKey('Payment Info.Payment ID'))
-    floor = db.Column('Floor', db.Integer, primary_key=True)
-    room_number = db.Column('Room Number', db.Integer, primary_key=True)
-    canceled = db.Column('Canceled', db.Integer)
+    canceled = db.Column('Canceled', db.Integer)  # may be used to keep record of canceled reservations 
 
 
 # Payment Info Table
